93ebcdac2116a874d956e67ae607a5724a25b09b
NekitKolomiichuk <kolomiych@gmail.com>
2015-11-15 16:51:32 +0200

    Dark Theme. First commit. v0.1

---
+++ myscript.js
@@ -0,0 +1,6 @@
+(function (){
+	
+    $('head').append('<style type="text/css"> #page_header .left {	background: url(' + chrome.extension.getURL('/images/iconm.png') + ') no-repeat !important; background-size: 100%; }</style>');
+ 	$('head').append('<style type="text/css"> #top_logo_down {	opacity: 0; }</style>');
+
+}());
